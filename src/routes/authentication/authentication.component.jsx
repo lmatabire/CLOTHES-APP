9e36d598994922d830e8beca1cece53b0434c70a
Lee Matabire <matabire23@gmail.com>
2022-05-16 11:09:54 -0500

    created authentication page component alongside sign in form

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -1,16 +1,19 @@
 // import { useEffect } from 'react';
 // import { getRedirectResult } from 'firebase/auth'
 
-import {
-    // auth,
-    signInWithGooglePopup,
-    // signInWithGoogleRedirect,
-    createUserDocumentFromAuth
-} from "../../utils/firebase/firebase.util";
+// import {
+//     // auth,
+//     signInWithGooglePopup,
+//     // signInWithGoogleRedirect,
+//     createUserDocumentFromAuth
+// } from "../../utils/firebase/firebase.util";
 
 import SignUpForm from '../../components/sign-up-form/sign-up-form.component'; 
+import SignInForm from '../../components/sign-in-form/sign-in-form.component';
 
-const SignIn = () => {
+import './authentication.styles.scss'
+
+const Authentication = () => {
     // useEffect(async () => {
     //     const response = await getRedirectResult(auth);
     //     console.log(response);
@@ -26,19 +29,19 @@ const SignIn = () => {
     // }, [])
 
 
-    const logGoogleUser = async () => {
-        const { user } = await signInWithGooglePopup();
-        const userDocRef = await createUserDocumentFromAuth(user);
-    };
+    // const logGoogleUser = async () => {
+    //     const { user } = await signInWithGooglePopup();
+    //     const userDocRef = await createUserDocumentFromAuth(user);
+    // };
 
     return (
-        <div>
-            <h1>Sign In Page</h1>
-            <button onClick={logGoogleUser}>Sing in with Google Popup</button>
+        <div className='authentication-container'>
+            {/* <button onClick={logGoogleUser}>Sing in with Google Popup</button> */}
             {/* <button onClick={signInWithGoogleRedirect}>Sing in with Google Redirect</button> */}
+            <SignInForm />
             <SignUpForm />
         </div>
     );
 };
 
-export default SignIn;
+export default Authentication;
